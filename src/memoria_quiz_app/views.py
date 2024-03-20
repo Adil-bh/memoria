1fb79ad36efb24e1f31685921347d9bc12a4c14d
Adil-bh <62136456+Adil-bh@users.noreply.github.com>
2024-03-20 10:33:06 +0100

    edit homepage and fixing activate method for signup

--- src/memoria_quiz_app/views.py
+++ src/memoria_quiz_app/views.py
@@ -47,7 +47,7 @@ def activate(request, uidb64, token):
     except:
         user = None
 
-    if User is not None and account_activation_token.check_token(user, token):
+    if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
 
